9e3cb2a7e8109a66d6ef2c155192cfd184e05df5
TedJenkler <Tedjenkler@gmail.com>
2024-01-24 05:54:01 -0200

    advice generator added

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const url = 'https://api.adviceslip.com/advice'
   return (
     <div className="bg-dark-blue w-screen h-screen flex justify-center items-center">
-      <div className="bg-grey-blue w-11/12 flex flex-col items-center text-center justify-center pt-10 px-6 pb-16">
+      <div className="relative bg-grey-blue w-11/12 h-80 flex flex-col items-center text-center justify-center pt-10 px-6 pb-16 rounded-lg md:w-1/2 lg:w-1/3">
         <Api />
       </div>
     </div>
--- src/app/features/api/Api.jsx
+++ src/app/features/api/Api.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import design from "../../../assets/design.png"
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchAdvice } from './adviceSlice'
 import { useEffect } from 'react'
+import dice from "../../../assets/dice.png"
 
 const Api = () => {
     const dispatch = useDispatch()
     const data = useSelector(state => state.advice.data)
+    const handleClick = () => {
+      dispatch(fetchAdvice())
+    }
     useEffect(() => {
         dispatch(fetchAdvice())
     }, [])
-    console.log(data)
   return (
     <>
         {data.isLoading ? <h1>Loading....</h1> :
         <>
-        <h2 className="text-neon text-xs font-extrabold tracking-[3.46px] mb-6">Advice #{data.slip.id}</h2>
-        <h1 className='text-cyan text-2xl font-extrabold tracking-[-0.26px] mb-6'>"{data.slip.advice}"</h1>
-        <img src={design} alt='design' />
+          <h2 className="text-neon text-xs font-extrabold tracking-[3.46px] mb-6">Advice #{data.slip.id}</h2>
+          <h1 className='text-cyan text-2xl font-extrabold tracking-[-0.26px] mb-6'>"{data.slip.advice}"</h1>
+          <img src={design} alt='design' />
+          <button onClick={handleClick} className='absolute bottom-[-28px] p-4 bg-neon rounded-full'><img src={dice} alt='dice' /></button>
         </>
         }
     </>
